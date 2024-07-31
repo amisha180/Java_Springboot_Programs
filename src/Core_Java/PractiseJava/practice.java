1d644c524de0940380290b3f0ca4938c7c8066a4
Amisha <amishakumari47939@gmail.com>
2024-07-31 09:56:41 +0530

    questions

--- src/Core_Java/PractiseJava/practice.java
+++ src/Core_Java/PractiseJava/practice.java
@@ -1,23 +1,7 @@
 package Core_Java.PractiseJava;
 
-
-
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-public class practice {
+public class practice{
     public static void main(String[] args) {
-
-       // IntStream.rangeClosed(2,4).forEach((x)-> System.out.println(x));
-        Stream.iterate(1,n->n*2).limit(5).forEach((x)-> System.out.println(x));
-        List<List<Integer>> nestedList = Arrays.asList(Arrays.asList(1,2,3),Arrays.asList(4,5),Arrays.asList(6,7,8,9));
-        List<Integer> flost = nestedList.stream().flatMap(List::stream).collect(Collectors.toList());
-        flost.stream().forEach((x)-> System.out.println(x));
 
     }
 }
